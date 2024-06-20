51adf5b4c7bd5f57581b81fa8daee60d260a2aa6
AdityaSwarup <43361986+AdityaSwarup@users.noreply.github.com>
2024-06-20 18:33:49 -0400

    Test cases added

--- app/geo_centroid/test/test_centroid_calculator.py
+++ app/geo_centroid/test/test_centroid_calculator.py
@@ -1,17 +1,74 @@
+import unittest
 from app.geo_centroid.src.centroid_calculator import CentroidCalculator
 
-# test 1: 64 Marshall st, Waterloo Ontario. Result: [43.4741752, -80.5204558] 
-# print(calculator.geocode_address("64 Marshall st, Waterloo Ontario"))
+class TestCentroidCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = CentroidCalculator("AIzaSyAGiZ1yqXYCFE2_TfC3q2VHSwgM0UrU10E")
+    
+    # There is a function called assertAlmostEqual in unittest, but not one for
+    # lists. This function is created for that purpose. 
+    def assertListAlmostEqual(self, list1, list2, tol):
+        self.assertEqual(len(list1), len(list2))
+        for a, b in zip(list1, list2):
+            self.assertAlmostEqual(a, b, tol)
+    #@classmethod
+    #def set_up_class(cls):
+     #   api_key = "AIzaSyAGiZ1yqXYCFE2_TfC3q2VHSwgM0UrU10E"
+      #  return cls(api_key)
+    
+    # The error tolerance on the latlng coordinates is up to 3 decimal places. 
+    # At 4 decimal places, the difference is 11.1m, basically negligible.     
+    def test_geocode_address(self):
+        result = self.calculator.geocode_address("64 Marshall st, Waterloo Ontario")
+        self.assertListAlmostEqual(result, [43.4741752, -80.5204558], 3)
+        
+    def test_get_centroid_all_addresses(self):
+        address_list_1 = ["64 Marshall st Waterloo Ontario", "CIF waterloo ontario", "King Street towers, waterloo ontario"]
+        result_1 = self.calculator.get_centroid(address_list_1)
+        self.assertListAlmostEqual(result_1, [43.47649843865154, -80.53145133903517, 3],3)
+        
+    def test_get_centroid_all_addresses_with_weight(self):
+        address_list_2 = [["64 Marshall st Waterloo Ontario", 5], "CIF waterloo ontario", "King Street towers, waterloo ontario"]
+        result_2 = self.calculator.get_centroid(address_list_2)
+        self.assertListAlmostEqual(result_2, [43.47580075938039, -80.540801226302, 7],3)
+        
+    def test_get_centroid_geocoded_addresses_with_weight(self):
+        address_list_3 = [[43.4740533, -80.5205138, 5], [43.475277, -80.54781349999999, 1], [43.4801632, -80.5260265, 1]]
+        result_3 = self.calculator.get_centroid(address_list_3)
+        self.assertListAlmostEqual(result_3, [43.47580075938039, -80.540801226302, 7],3)
+        
+    def test_get_centroid_single_address(self):
+        address_list_4 = [[43.4740533, -80.5205138, 5]]
+        result_4 = self.calculator.get_centroid(address_list_4)
+        self.assertIsNone(result_4)
+        
+    def test_get_centroid_mix_address_and_geocoded(self):
+        address_list_5 = [["64 Marshall st Waterloo Ontario", 5], [43.475277, -80.54781349999999, 1], "King Street towers, waterloo ontario"]
+        result_5 = self.calculator.get_centroid(address_list_5)
+        self.assertListAlmostEqual(result_5, [43.47580075938039, -80.540801226302, 7],3)
+        
+    def test_get_centroid_mix_weighted_and_non_weighted(self):
+        address_list_6 = ["64 Marshall st Waterloo Ontario", [43.475277, -80.54781349999999, 2], "King Street towers, waterloo ontario"]
+        result_6 = self.calculator.get_centroid(address_list_6)
+        self.assertListAlmostEqual(result_6, [43.47619329770745, -80.53554194128088, 4],3)
+        
+    def test_get_centroid_fake_address(self):
+        address_list_7 = ["fake address", [43.475277, -80.54781349999999, 2], "King Street towers, waterloo ontario"]
+        result_7 = self.calculator.get_centroid(address_list_7)
+        self.assertListAlmostEqual(result_7, [43.4785349262818, -80.53328922483436, 3],3)
+        
+    def test_get_centroid_invalid_latlng_list(self):
+        address_list_8 = ["64 Marshall st Waterloo Ontario", [-80.54781349999999], "King Street towers, waterloo ontario"]
+        result_8 = self.calculator.get_centroid(address_list_8)
+        self.assertListAlmostEqual(result_8, [43.47710828310337, -80.52327001064637, 2],3)
+        
+    def test_get_centroid_invalid_latlng_coord(self):
+        address_list_9 = ["64 Marshall st Waterloo Ontario", [443.475277, -80.54781349999999, 2], "King Street towers, waterloo ontario"]
+        result_9 = self.calculator.get_centroid(address_list_9)
+        self.assertListAlmostEqual(result_9, [43.47710828310337, -80.52327001064637, 2],3)
+        
+    def test_get_centroid_invalid_weight(self):
+        address_list_10 = [["64 Marshall st Waterloo Ontario", 5.1], [43.475277, -80.54781349999999, 1]]
+        result_10 = self.calculator.get_centroid(address_list_10)
+        self.assertIsNone(result_10)
 
-# test2:
-# address_list = ["64 Marshall st Waterloo Ontario", "CIF waterloo ontario", "King Street towers, waterloo ontario"]  # Provide your address list here
-# calculator = CentroidCalculator(api_key)
-# # print(calculator.geocode_address("india"))
-# centroid = calculator.get_centroid(address_list) Result:[[43.47649843865154, -80.53145133903517, 3]]
-
-# test 3: all same as above
-# address_list = [["64 Marshall st Waterloo Ontario", 5], "CIF waterloo ontario", "King Street towers, waterloo ontario"] 
-# [[43.47580075938039, -80.540801226302, 7]]
-
-# test4: all same and same results
-# address_list = [[43.4740533, -80.5205138, 5], [43.475277, -80.54781349999999, 1], [43.4801632, -80.5260265, 1]]
